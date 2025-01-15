02b1ec8fe4dafa0508a593ce3f0f4e8ab665b3c5
Rodrigo-sartori <rodrigosartoriazevedo@gmail.com>
2025-01-15 16:51:38 -0300

    add port variable

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const dairyController = require("./controller/dairyController");
+const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
 app.use("/", dairyController);
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("aplicação iniciada");
 });
