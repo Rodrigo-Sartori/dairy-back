63b8b0bfc22563610004f1af2e4259e3599d572a
Rodrigo-sartori <rodrigosartoriazevedo@gmail.com>
2025-08-13 11:13:22 -0300

    altera health check como endpoint publico

--- src/main/java/com/dairy/dairy_back/config/SecurityConfig.java
+++ src/main/java/com/dairy/dairy_back/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
